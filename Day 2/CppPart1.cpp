9b0d5cc94f7a20724b60d2853482b3f75e8d7629
Gabriel Mocioaca <gabriel.mocioaca@gmail.com>
2019-12-02 07:43:16 +0200

    Cpp solution Day 2

---
+++ Day 2/CppPart1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+ifstream in("input.txt");
+
+int main() {
+	vector<int> v;
+	int x;
+	while (in >> x) {
+		v.push_back(x);
+	}
+
+	v[1] = 12;
+	v[2] = 2;
+	int i = 0;
+	while (v[i] != 99) {
+		if (v[i] == 1) {
+			v[v[i + 3]] = v[v[i + 1]] +v[ v[i + 2]];
+		}
+		if (v[i] == 2) {
+			v[v[i + 3]] = v[v[i + 1]] * v[v[i + 2]];
+		}
+		i += 4;
+	}
+
+	cout << v[0];
+}
---
+++ Day 2/CppPart2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+ifstream in("input.txt");
+
+int main() {
+	vector<int> w;
+	int x;
+	while (in >> x) {
+		w.push_back(x);
+	}
+
+	for (int k = 0; k < 100; k++) {
+		for (int l = 0; l < 100; l++) {
+			vector<int> v = w;
+			v[1] = k;
+			v[2] = l;
+			int i = 0;
+			while (v[i] != 99) {
+				if (v[i] > v.size() || v[i + 1] > v.size() || v[i + 2] > v.size() || v[i + 3] > v.size())
+					break;
+				if (v[i] == 1) {
+					v[v[i + 3]] = v[v[i + 1]] + v[v[i + 2]];
+				}
+				if (v[i] == 2) {
+					v[v[i + 3]] = v[v[i + 1]] * v[v[i + 2]];
+				}
+				i += 4;
+			}
+			if (v[i] > v.size() || v[i + 1] > v.size() || v[i + 2] > v.size() || v[i + 3] > v.size())
+				continue;
+
+			if (v[0] == 19690720)
+				cout << k << ' ' << l;
+		}
+	}
+
+	
+}
